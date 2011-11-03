9dd2a399d78e57e34ef4859b290943151fc90803
pai982010j90 <pai982010j90@gmail.com>
2011-11-03 13:20:30 +0100

    Propuesta v 03-11-11, rev 06 - Implementacion MODELO_A_FICHERO_TEXTO

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -4,6 +4,10 @@ import Controlador.TipoEvento;
 import Modelo.ObjetoAstronomicoEsferico;
 import Modelo.SistemaPlanetario;
 import Vista.VistaTexto;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.lang3.text.WordUtils;
 
 /**
@@ -21,18 +25,18 @@ public class Controlador {
     }
 
     public void procesadorEvento(TipoEvento evento) {
-        System.err.println("Evento:"+evento);
+        System.err.println("Evento:" + evento);
 
         switch (evento) {
             case MOSTRAR_SISTEMA_PLANETARIO:
                 //System.out.println("Mostrando Sistema Planetario");
                 vista.mostrarSistemaPlanetario(sPlanetario);
                 break;
-                
+
             case MOSTRAR_SISTEMA_PLANETARIO_RESUMEN:
                 //System.out.println("Mostrando Sistema Planetario");
                 vista.mostrarSistemaPlanetarioResumen(sPlanetario);
-                break;                
+                break;
             case MOSTRAR_OBJETO_ASTRONOMICO_ESFERICO:
                 //System.out.println("Mostrando cualquier tipo de objeto Esferico");
                 String nombreObjeto = WordUtils.capitalizeFully(vista.getValor("Nombre del objeto"));
@@ -44,13 +48,23 @@ public class Controlador {
                     //System.out.println("'" + nombreObjeto + "' no encontrado en el sistema planetario '" + sPlanetario.getNombre() + "'");
                 }
                 break;
+            case MODELO_A_FICHERO_TEXTO:
+                String nombreFichero = vista.getValor("Nombre del fichero");
+        try {
+            FileWriter fw = new FileWriter(nombreFichero);
+            fw.write("Nombre del sistema planetario:"+sPlanetario.getNombre()+"\n");
+            for(ObjetoAstronomicoEsferico o:sPlanetario.getObjetosEsfericos().values()){
+               fw.write("Objeto:"+o.getNombre()+" es un "+o.getClass().getSimpleName() +"\n"); 
+            }
+            fw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Controlador.class.getName()).log(Level.SEVERE, null, ex);
+        }
                 
+                break;
+
             default:
-                
-                System.err.println("Controlador.procesadorEvento(): '"+evento+"' no controlado");
-
-
-
+                System.err.println("Controlador.procesadorEvento(): '" + evento + "' no controlado");
         }
     }
 }
